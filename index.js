63006cd3b08a306bc57516c672c0f95763321a9c
manit-local <mnit72@gmail.com>
2024-04-29 19:56:21 +0700

    update index file

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ const app = express();
 app.get('/', (req, res) => {
     res.send({ 
         "id": 1,
-        "name": "Manit" 
+        "name": "Manit",
+        "id": 2,
+        "name": "Mana" 
     });
 });
 
-app.listen(7000, () => {
-    console.log('Express Server is Running at Port No 7000..'); 
+app.listen(5000, () => {
+    console.log('Express Server is Running at Port No 5000..'); 
 });
